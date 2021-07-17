359cd63e891de18356787a6fb5406e2e485ea862
KSteuer <konstantin.steuer>
2021-07-17 13:44:48 +0200

    again

--- Spring boot Backend/organicer/src/main/java/com/web/organicer/faq/FaqService.java
+++ Spring boot Backend/organicer/src/main/java/com/web/organicer/faq/FaqService.java
@@ -17,7 +17,7 @@ public class FaqService {
 
     public String postFaq(Faq faq){
         if (faq.getId()==null){
-            if (!faqRepository.findByQuestion(faq.getQuestion()).isEmpty()){
+            if (!faqRepository.findByQuestion(faq.getFrage()).isEmpty()){
                 return "Frage existiert bereits";
             }
             return addNewFaq(faq);
