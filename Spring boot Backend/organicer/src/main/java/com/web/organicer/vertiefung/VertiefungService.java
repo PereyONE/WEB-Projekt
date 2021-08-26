24c2f5f994c3b862a7bbe250a34db81de93b2ab0
KSteuer <konstantin.steuer>
2021-08-26 17:42:40 +0200

    Vertiefungen sind Fertig + Kommentare

--- Spring boot Backend/organicer/src/main/java/com/web/organicer/vertiefung/VertiefungService.java
+++ Spring boot Backend/organicer/src/main/java/com/web/organicer/vertiefung/VertiefungService.java
@@ -2,7 +2,6 @@ package com.web.organicer.vertiefung;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -11,11 +10,12 @@ public class VertiefungService {
 
     private final VertiefungRepository vertiefungRepository;
 
+    //Alle Vertiefungsfächer werden aus der Datenbank abgefragt
     public List<Vertiefung> getVertiefung(){
-
         return vertiefungRepository.findAll();
     }
 
+    //Anlegen oder Updaten eines neuen bzw. alten Vertiefungsfaches
     public String postVertiefung(Vertiefung vertiefung){
         if(vertiefung.getId()==null){
             if(!vertiefungRepository.findByName(vertiefung.getName()).isEmpty()){
@@ -26,25 +26,27 @@ public class VertiefungService {
         return updateVertiefung(vertiefung);
     }
 
+    //Abspeichern eines neuen Vertiefungsfaches
     public String addVertiefung(Vertiefung vertiefung){
         vertiefungRepository.save(vertiefung);
         return "Vertiefungsrichtung " + vertiefung.getName()+ " wurde hinzugefügt";
     }
 
+    //Aktualisieren eines bestehenden Vertiefungsfaches
     public String updateVertiefung(Vertiefung vertiefung){
         vertiefungRepository.save(vertiefung);
             return "Vertiefungsrichtung " + vertiefung.getName()+ " wurde aktualisiert";
 
     }
 
+    //Löschen eines Vertiefungsfaches aus der Datenbank
     public String deleteVertiefung(Vertiefung vertiefung) {
         if (vertiefung.getId() == null) {
-            return "Keine Vertiefungs Id";
+            return "Vertiefung konnte nicht gelöscht werden";
         }
         vertiefungRepository.delete(vertiefung);
         return "Vertiefungsrichtung " + vertiefung.getName()+ " wurde gelöscht";
     }
-
 }
 
 
