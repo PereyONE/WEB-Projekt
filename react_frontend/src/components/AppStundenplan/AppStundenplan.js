dd3fe32e1277901d8938db2fcf173e779895c5b4
Justus Bialojahn <justus.bialojahn@smail.th-koeln.de>
2021-09-13 20:19:29 +0200

    admin Termine und Stundenplan

--- react_frontend/src/components/AppStundenplan/AppStundenplan.js
+++ react_frontend/src/components/AppStundenplan/AppStundenplan.js
@@ -6,139 +6,17 @@ import Ereignis from'./Ereignis';
 import Popup from'./Popup';
 import { Redirect } from 'react-router';
 import axios from 'axios';
+import Stundenplan from './Stundenplan';
 
-const Tage = () => {
-  const [eintraege, seteintraege] = useState([]);
 
-  useEffect(()=> {
-    axios.get('/api/termine')
-    .then(res => {
-      seteintraege(res.data)
-    })
-    .catch(function (error) {
-      console.log(error)
-      if (error.response.status===403) {
-        localStorage.clear()
-        window.location.reload()
-      }
-    })
-  },[])
-  
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 700;
-  React.useEffect(() => {
-   const handleResizeWindow = () => setWidth(window.innerWidth);
-    // subscribe to window resize event "onComponentDidMount"
-    window.addEventListener("resize", handleResizeWindow);
-    return () => {
-      // unsubscribe "onComponentDestroy"
-      window.removeEventListener("resize", handleResizeWindow);
-    };
-  }, []);
-  if (width > breakpoint) {
-    return (
-      <Row className="justify-content-md-center" sm={5}>
-        <div className="tag1">
-          Montag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Montag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-        <div className="tag2">
-          Dienstag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Dienstag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-        <div className="tag2">
-          Mittwoch<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Mittwoch"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-        <div className="tag2">
-          Donnerstag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Donnerstag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-        <div className="tag3">
-          Freitag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Freitag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Row>
-    );
-  }
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <div className="tag4">
-          Montag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Montag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="tag4">
-          Dienstag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Dienstag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="tag4">
-          Mittwoch<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Mittwoch"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="tag4">
-          Donnerstag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Donnerstag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="tag4">
-          Freitag<br></br>
-          {eintraege.map((eintrag, i) => {
-            if(eintrag.wochentag == "Freitag"){
-              return(<Ereignis eintrag={eintrag} index={i} />)
-            }
-          })}
-        </div>
-      </Carousel.Item>
-    </Carousel>
-    
-  );
-}
 
 function AppStundenplan({auth}) {
+  const [semester, setsemester] = useState("1");
+
+  const handleChange = e => {
+    setsemester(e.target.value)
+    console.log(semester)
+  };
 
   if (!auth) {
     return <Redirect to="/login" />
@@ -152,20 +30,20 @@ function AppStundenplan({auth}) {
         <h1>Stundenplan</h1>
         <div></div>
         <div className="inhaltRechts">
-          <select className="dropdown1">
-                <option value="1.Semester">1.Semester</option>
-                <option value="2.Semester">2.Semester</option>
-                <option value="3.Semester">3.Semester</option>
-                <option value="4.Semester">4.Semester</option>
-                <option value="5.Semester">5.Semester</option>
-                <option value="6.Semester">6.Semester</option>
-                <option value="7.Semester">7.Semester</option>
+          <select className="dropdown1" onChange={handleChange}>
+                <option value="1">1.Semester</option>
+                <option value="2">2.Semester</option>
+                <option value="3">3.Semester</option>
+                <option value="4">4.Semester</option>
+                <option value="5">5.Semester</option>
+                <option value="6">6.Semester</option>
+                <option value="7">7.Semester</option>
           </select>
         </div>
-        <div className="inhaltRechts"><Popup/></div>
+        <div className="inhaltRechts"><Popup semester={semester}/></div>
         
       </Row>
-      <Tage/>
+      <Stundenplan semester={semester}/>
       
     </div>
     
---
+++ react_frontend/src/components/AppStundenplan/Stundenplan.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import './AppStundenplan.css';
+import { Row } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
+import Ereignis from './Ereignis';
+import axios from 'axios';
+
+function Stundenplan(semester) {
+    const [eintraege, seteintraege] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('/api/termine')
+            .then(res => {
+                seteintraege(res.data)
+                console.log(eintraege)
+            })
+            .catch(function (error) {
+
+                if (error.response.status === 403) {
+                    localStorage.clear()
+                    window.location.reload()
+                }
+            })
+    }, [])
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 700;
+    React.useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+        // subscribe to window resize event "onComponentDidMount"
+        window.addEventListener("resize", handleResizeWindow);
+        return () => {
+            // unsubscribe "onComponentDestroy"
+            window.removeEventListener("resize", handleResizeWindow);
+        };
+    }, []);
+    if (width > breakpoint) {
+        return (
+            <Row className="justify-content-md-center" sm={5}>
+                <div className="tag1">
+                    Montag<br></br>
+                    {eintraege.map((eintrag, i) => {
+
+                        if ((eintrag.wochentag === "Montag") && (eintrag.semester === semester.semester)) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+                <div className="tag2">
+                    Dienstag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Dienstag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+                <div className="tag2">
+                    Mittwoch<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Mittwoch" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+                <div className="tag2">
+                    Donnerstag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Donnerstag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+                <div className="tag3">
+                    Freitag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Freitag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Row>
+        );
+    }
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <div className="tag4">
+                    Montag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Montag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="tag4">
+                    Dienstag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Dienstag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="tag4">
+                    Mittwoch<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Mittwoch" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="tag4">
+                    Donnerstag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Donnerstag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="tag4">
+                    Freitag<br></br>
+                    {eintraege.map((eintrag, i) => {
+                        if (eintrag.wochentag == "Freitag" && eintrag.semester == semester.semester) {
+                            return (<Ereignis eintrag={eintrag} index={i} semester={semester} />)
+                        }
+                    })}
+                </div>
+            </Carousel.Item>
+        </Carousel>
+
+    );
+}
+export default Stundenplan;
